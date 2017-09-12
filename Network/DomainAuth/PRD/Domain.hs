c3a9caff4e7b9d422434d2e6d9edc23c1c4a78e5
Kazu Yamamoto <kazu@iij.ad.jp>
2017-09-12 16:18:53 +0900

    doctest for PRD.

--- Network/DomainAuth/PRD/Domain.hs
+++ Network/DomainAuth/PRD/Domain.hs
@@ -6,9 +6,16 @@ import Network.DomainAuth.PRD.Lexer
 import Text.Appar.ByteString
 import qualified Data.ByteString.Char8 as BS
 
-{-|
-  Extract a domain from a value of a header field.
--}
+-- | Extract a domain from a value of a header field.
+--
+-- >>> extractDomain "Alice Brown <alice.brown@example.com>"
+-- Just "example.com"
+-- >>> extractDomain "\"Alice . Brown\" <alice.brown@example.com> (Nickname here)"
+-- Just "example.com"
+-- >>> extractDomain "alice.brown@example.com"
+-- Just "example.com"
+-- >>> extractDomain "Alice Brown <example.com>"
+-- Nothing
 
 extractDomain :: RawFieldValue -> Maybe Domain
 extractDomain bs = parse structured bs >>= takeDomain
