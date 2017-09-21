5b2b94009e88750171195160ae83d3a68e79741d
Kazu Yamamoto <kazu@iij.ad.jp>
2017-09-21 13:51:54 +0900

    using ASN1 libraries.

--- Network/DomainAuth/Pubkey/RSAPub.hs
+++ Network/DomainAuth/Pubkey/RSAPub.hs
@@ -1,33 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Network.DomainAuth.Pubkey.RSAPub where
+module Network.DomainAuth.Pubkey.RSAPub (
+    lookupPublicKey
+  ) where
 
-import Crypto.PubKey.RSA
+import Crypto.PubKey.RSA (PublicKey)
+import Data.ASN1.BinaryEncoding (DER)
+import Data.ASN1.Encoding (decodeASN1')
+import Data.ASN1.Types (fromASN1)
 import Data.ByteString (ByteString)
-import qualified Data.ByteString as BS (foldl', dropWhile, length, tail)
 import qualified Data.ByteString.Char8 as BS ()
-import qualified Data.ByteString.Lazy as BL
+import Data.X509 (PubKey(PubKeyRSA))
 import Network.DNS (Domain)
-import qualified Network.DNS as DNS hiding (Domain)
+import qualified Network.DNS as DNS
 import Network.DomainAuth.Mail
 import qualified Network.DomainAuth.Pubkey.Base64 as B
-import qualified Network.DomainAuth.Pubkey.Der as D
 
 -- $setup
 -- >>> import Network.DNS
 
-lookupPublicKey :: DNS.Resolver -> Domain -> IO (Maybe PublicKey)
-lookupPublicKey resolver domain = decode <$> lookupPublicKey' resolver domain
-  where
-    decode = (>>= return . decodeRSAPublicyKey)
-
--- |
+-- | Looking up an RSA public key
 --
 -- >>> rs <- DNS.makeResolvSeed DNS.defaultResolvConf
--- >>> withResolver rs $ \rslv -> lookupPublicKey' rslv "dk200510._domainkey.yahoo.co.jp"
--- Just "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCxSYNH3flPtGnDPuCKsuZ1VJD/sToRfS38qMT/YrOvTguUABLeMGheCTg7S3HGas+JEOcxEsYuOPxYULGogqzJ8WKpwlh4Gdz+fcaPstKcsUZvurAfLmudvLtYtabuaHyUH586GzAW7WWf80rvVR58NlFf+n8tyGZvlc6MpaVv1QIDAQAB"
--- >>> withResolver rs $ \rslv -> lookupPublicKey' rslv "20161025._domainkey.gmail.com"
--- Just "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAviPGBk4ZB64UfSqWyAicdR7lodhytae+EYRQVtKDhM+1mXjEqRtP/pDT3sBhazkmA48n2k5NJUyMEoO8nc2r6sUA+/Dom5jRBZp6qDKJOwjJ5R/OpHamlRG+YRJQqR\222tqEgSiJWG7h7efGYWmh4URhFM9k9+rmG/CwCgwx7Et+c8OMlngaLl04/bPmfpjdEyLWyNimk761CX6KymzYiRDNz1MOJOJ7OzFaS4PFbVLn0m5mf0HVNtBpPwWuCNvaFVflUYxEyblbB6h/oWOPGbzoSgtRA47SHV53SwZjIsVpbq4LxUW9IxAEwYzGcSgZ4n5Q8X8TndowsDUzoccPFGhdwIDAQAB"
+-- >>> withResolver rs $ \rslv -> lookupPublicKey rslv "dk200510._domainkey.yahoo.co.jp"
+-- Just (PublicKey {public_size = 128, public_n = 124495277115430906234131617223399742059624761592171426860362133400468320289284068350453787798555522712914036293436636386707903510390018044090096883314714401752103035965668114514933570840775088208966674120428191313530595210688523478828022953238411688594634270571841869051696953556782155414877029327479844990933, public_e = 65537})
+-- >>> withResolver rs $ \rslv -> lookupPublicKey rslv "20161025._domainkey.gmail.com"
+-- Just (PublicKey {public_size = 256, public_n = 24002918530496096406691035681124918576525139397315303508411989678485471042094243709057313703002506577481962009015943359911855470236184740304869372300375695346466126498491672986773463137306714366250125164042667360646066116764211863426455899157774209331432246892273100824803003088472583507901403747123280231573655315087668569169140244507741538460717772392364103610973022227332742456151275783849571119048507254523999452581754451573432729267867937880516609319975372273089173378196646435625090021348583100824870283641053888848697729241628460935831026221013673635926904278136940569963717548356154570137751386534390787055991, public_e = 65537})
+lookupPublicKey :: DNS.Resolver -> Domain -> IO (Maybe PublicKey)
+lookupPublicKey resolver domain = do
+    mpub <- lookupPublicKey' resolver domain
+    return $ case mpub of
+      Nothing  -> Nothing
+      Just pub -> Just $ decodeRSAPublicyKey pub
+
 lookupPublicKey' :: DNS.Resolver -> Domain -> IO (Maybe ByteString)
 lookupPublicKey' resolver domain = do
     ex <- DNS.lookupTXT resolver domain
@@ -39,17 +44,8 @@ extractPub :: [ByteString] -> Maybe ByteString
 extractPub = lookup "p" . parseTaggedValue . head
 
 decodeRSAPublicyKey :: ByteString -> PublicKey
-decodeRSAPublicyKey bs = PublicKey size n e
+decodeRSAPublicyKey b64 = pub
   where
-    subjectPublicKeyInfo = D.decode . B.decode' $ bs
-    [_, subjectPublicKey] = D.tlv subjectPublicKeyInfo
-    rsaPublicKey = D.decode . toLazy . bitString . D.cnt $ subjectPublicKey
-    [bn',be'] = D.tlv rsaPublicKey
-    bn = BS.dropWhile (== 0) $ D.cnt bn'
-    be = D.cnt be'
-    n = toNum bn
-    e = toNum be
-    size = fromIntegral . BS.length $ bn
-    toNum = BS.foldl' (\x y -> x*256 + fromIntegral y) 0
-    bitString = BS.tail
-    toLazy x = BL.fromChunks [x]
+    der = B.decode b64
+    Right ans1 = decodeASN1' (undefined :: DER) der
+    Right (PubKeyRSA pub,[]) = fromASN1 ans1
